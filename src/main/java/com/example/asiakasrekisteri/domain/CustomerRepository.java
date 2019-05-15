3bdd69597c81f7de7f9dcf9f39944fe7321eea7e
knuutton <toni.knuuttila@outlook.com>
2019-05-15 23:35:16 +0300

    versio 0.6

--- src/main/java/com/example/asiakasrekisteri/domain/CustomerRepository.java
+++ src/main/java/com/example/asiakasrekisteri/domain/CustomerRepository.java
@@ -9,6 +9,9 @@ import com.example.asiakasrekisteri.model.Customer;
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
 	List<Customer> findById(long customerId);
+	List<Customer> findByCity(String city);
+	List<Customer> findByRestaurant(String restaurant);
+	List<Customer> findByCompany(String company);
 	
 	
 }
